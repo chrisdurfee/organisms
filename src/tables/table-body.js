3a320f399191320496f732d70a9472cb91f0fa68
tech_e <chris.durfee@dentalqore.com>
2024-11-03 18:32:58 -0700

    Bump version to 1.0.36 and add TableBody component to table utilities

---
+++ src/tables/table-body.js
@@ -0,0 +1,29 @@
+import { Tbody } from '@base-framework/atoms';
+import { List } from '../lists/list.js';
+
+/**
+ * TableBody
+ *
+ * This will create a table body component.
+ *
+ * @property {string} class - The class to add to the list
+ * @property {string} key - The key to use to identify the items
+ * @property {array} [items] - The items
+ *
+ * @class
+ */
+// @ts-ignore
+export class TableBody extends List
+{
+    render()
+    {
+        // @ts-ignore
+        const rowCallBack = this.row.bind(this);
+
+        return Tbody({
+            // @ts-ignore
+            class: `tbody ${this.class || ''}`,
+            for: ['items', rowCallBack]
+        });
+    }
+};
