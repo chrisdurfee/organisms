e135d00a33fe96e32ccc7d97bc0d08383ff44bd0
tech_e <chris.durfee@dentalqore.com>
2024-12-27 16:58:56 -0700

    Update List class documentation to specify return type as ComponentConstructor

--- src/lists/list.js
+++ src/lists/list.js
@@ -21,7 +21,7 @@ const clone = (data) => JSON.parse(JSON.stringify(data));
  * @property {string} key - The key to use to identify the items
  * @property {array} [items] - The items
  *
- * @returns {Constructor}
+ * @returns {ComponentConstructor}
  */
 export const List = Jot(
 {
