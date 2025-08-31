f36b85a5799cd858356fcf6fd51e87e3f81aa824
tech_e <chris.durfee@dentalqore.com>
2025-08-31 17:45:30 -0600

    Refactor NavLink component to improve rendering and watcher setup

--- src/router/nav-link.js
+++ src/router/nav-link.js
@@ -1,4 +1,3 @@
-import { A } from '@base-framework/atoms';
 import { Component, router } from '@base-framework/base';
 
 /**
@@ -43,128 +42,130 @@ const iSActive = (path, url) => new RegExp('^' + path + '($|#|/|\\.).*').test(ur
  */
 export class NavLink extends Component
 {
-	/**
-	 * This will configure the link active class.
-	 *
-	 * @protected
-	 * @returns {void}
-	 */
-	beforeSetup()
-	{
-		// @ts-ignore
-		this.selectedClass = this.activeClass || 'active';
-	}
+    /**
+     * This will configure the link active class.
+     *
+     * @protected
+     * @returns {void}
+     */
+    beforeSetup()
+    {
+        // @ts-ignore
+        this.selectedClass = this.activeClass || 'active';
+    }
 
-	/**
-	 * This will render the component.
-	 *
-	 * @returns {object}
-	 */
-	render()
-	{
-		// @ts-ignore
-		const href = this.href,
-		// @ts-ignore
-		text = this.text,
-		watchers = this.setupWatchers(href, text);
+    /**
+     * This will render the component.
+     *
+     * @returns {object}
+     */
+    render()
+    {
+        // @ts-ignore
+        const href = this.href,
+        // @ts-ignore
+        text = this.text,
+        watchers = this.setupWatchers(href, text);
 
-		return A({
-			cache: 'link',
-			// @ts-ignore
-			class: this.class || this.className || null,
-			onState: ['selected', {
-				[this.selectedClass]: true
-			}],
-			href: this.getString(href),
-			text: this.getString(text),
-			nest: this.nest || this.children,
-			watch: watchers
-		});
-	}
+        return {
+            tag: 'a',
+            // @ts-ignore
+            class: this.class || this.className || null,
+            onState: ['selected', {
+                [this.selectedClass]: true
+            }],
+            href: this.getString(href),
+            text: this.getString(text),
+            nest: this.nest || this.children,
+            // @ts-ignore
+            dataSet: this.dataSet,
+            watch: watchers
+        };
+    }
 
-	/**
-	 * This will get the link path.
-	 *
-	 * @returns {string|null}
-	 */
-	getLinkPath()
-	{
-		// @ts-ignore
-		return this?.link?.pathname || null;
-	}
+    /**
+     * This will get the link path.
+     *
+     * @returns {string|null}
+     */
+    getLinkPath()
+    {
+        // @ts-ignore
+        return this?.panel?.pathname || null;
+    }
 
-	/**
-	 * This will get string.
-	 *
-	 * @param {string} string
-	 * @returns {(string|null)}
-	 */
-	getString(string)
-	{
-		const type = typeof string;
-		return (type !== 'object' && type !== 'undefined')? string : null;
-	}
+    /**
+     * This will get string.
+     *
+     * @param {string} string
+     * @returns {(string|null)}
+     */
+    getString(string)
+    {
+        const type = typeof string;
+        return (type !== 'object' && type !== 'undefined')? string : null;
+    }
 
-	/**
-	 * This will setup the watchers.
-	 *
-	 * @protected
-	 * @param {string} href
-	 * @param {string} text
-	 * @returns {array}
-	 */
-	setupWatchers(href, text)
-	{
-		// @ts-ignore
-		const exact = (this.exact === true),
-		data = router.data;
+    /**
+     * This will setup the watchers.
+     *
+     * @protected
+     * @param {string} href
+     * @param {string} text
+     * @returns {array}
+     */
+    setupWatchers(href, text)
+    {
+        // @ts-ignore
+        const exact = (this.exact === true),
+        data = router.data;
 
-		const watchers = [];
+        const watchers = [];
 
-		if (href && typeof href === 'object')
-		{
-			watchers.push(Watcher('href', href));
-		}
+        if (href && typeof href === 'object')
+        {
+            watchers.push(Watcher('href', href));
+        }
 
-		if (text && typeof text === 'object')
-		{
-			watchers.push(Watcher('text', text));
-		}
+        if (text && typeof text === 'object')
+        {
+            watchers.push(Watcher('text', text));
+        }
 
-		watchers.push({
-			value: ['[[path]]', data],
-			callBack: (value, ele) =>
-			{
-				const path = ele.pathname + ele.hash;
+        watchers.push({
+            value: ['[[path]]', data],
+            callBack: (value, ele) =>
+            {
+                const path = ele.pathname + ele.hash;
 				const selected = exact? (value === path) : (iSActive(ele.pathname, value));
-				this.update(selected);
-			}
-		});
+                this.update(selected);
+            }
+        });
 
-		return watchers;
-	}
+        return watchers;
+    }
 
-	/**
-	 * This will setup the states.
-	 *
-	 * @protected
-	 * @returns {object}
-	 */
-	setupStates()
-	{
-		return {
-			selected: false
-		};
-	}
+    /**
+     * This will setup the states.
+     *
+     * @protected
+     * @returns {object}
+     */
+    setupStates()
+    {
+        return {
+            selected: false
+        };
+    }
 
-	/**
-	 * This will update the class on the element.
-	 *
-	 * @param {boolean} selected
-	 * @returns {void}
-	 */
-	update(selected)
-	{
-		this.state.selected = selected;
-	}
+    /**
+     * This will update the class on the element.
+     *
+     * @param {boolean} selected
+     * @returns {void}
+     */
+    update(selected)
+    {
+        this.state.selected = selected;
+    }
 }
